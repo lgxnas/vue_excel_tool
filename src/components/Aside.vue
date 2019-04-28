d77a79e6150de11c948cd6c618f01734f877f0ff
lgxlyh <lgxlyh@gmail.com>
2019-04-28 16:57:53 +0800

    aside test

---
+++ src/components/Aside.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="aside">
+    <span class="title">Xlsx Extend</span>
+    <el-menu :default-openeds="['1', '2']">
+      <el-submenu index="1">
+        <template slot="title">标准查询</template>
+        <el-menu-item-group>
+          <el-menu-item index="1-1">地表水</el-menu-item>
+          <el-menu-item index="1-2">地下水</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-submenu index="2">
+        <template slot="title">数据分析</template>
+        <el-menu-item-group>
+          <el-menu-item index="2-1">地表水</el-menu-item>
+          <el-menu-item index="2-2">地下水</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+    </el-menu>
+</div>
+</template>
+
+<style>
+span{
+    line-height: 60px;
+}
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <el-row>
+        <el-col :span="6"><div>Xlsx Extend dev by Mr.Li</div></el-col>
+    </el-row>
+</template>
+
+<style>
+    .el-row{
+        line-height: 60px;
+    }
+    .el-col{
+        text-align: left;
+    }
+</style>
+
